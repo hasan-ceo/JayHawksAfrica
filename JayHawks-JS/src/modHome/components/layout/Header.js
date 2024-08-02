1020402b7cef444ff86a16947dfe03f3143304ed
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2024-08-02 16:57:05 +0600

    hero page and logo add

--- JayHawks-JS/src/modHome/components/layout/Header.js
+++ JayHawks-JS/src/modHome/components/layout/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="max-w-full px-2 sm:px-8 lg:px-[120px] flex">
       <Link to="/" className="flex items-center lg:items-start py-2">
-        <img className="h-10" src="/images/logo.png" alt="logo" />
+        <img className="h-10" src="/images/hero1.jpg" alt="logo" />
       </Link>
     </div>
   );
--- JayHawks-JS/src/modHome/landing/Landing.js
+++ JayHawks-JS/src/modHome/landing/Landing.js
@@ -2,12 +2,29 @@ import React from "react";
 
 export default function Landing() {
   return (
-    <div className="max-w-full px-2 sm:px-8 lg:px-[120px] grid grid-cols-1 md:grid-cols-2">
-      <div className="text-white grid">
-        <h1 className="font-bold text-2xl lg:text-4xl my-4">
-          Under construction
-        </h1>
+    // <div className="overflow-y-off">
+    <section className="text-gray-900 no-scrollbar">
+      <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+        <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+          <img
+            src="/images/hero1.jpg"
+            alt=""
+            className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+          />
+        </div>
+        <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+          <h1 className="text-5xl text-white font-bold leading-none sm:text-6xl">
+            Jayhawks Consulting Africa
+          </h1>
+          <p className="mt-6 mb-8  text-white text-lg sm:mb-12">
+            Grow Your Businesswith
+            {/* <br className="hidden md:inline lg:hidden" /> */}
+            <br />
+            Website Coming Soon.......
+          </p>
+        </div>
       </div>
-    </div>
+    </section>
+    // </div>
   );
 }
